7af2777b58cf434176ed8ee473918628b1a26924
joao <joao_loba@sapo.pt>
2022-10-18 22:54:45 +0000

    refactors duck.nvim extended plugin

--- lua/user/duck-hatch.lua
+++ lua/user/duck-hatch.lua
@@ -19,5 +19,5 @@ function KillDucks(quantity)
   for i=quantity,1,-1 do require('duck').cook() end
 end
 
-vim.api.nvim_create_user_command("Hatch", ":lua require('duck').hatch(<f-args>)", {nargs = '?', complete = "customlist,v:lua.Eggs" })
-vim.api.nvim_create_user_command("HatchKill", ":lua KillDucks(<f-args>)", {nargs = '?'})
+vim.api.nvim_create_user_command("Hatch", ":lua require('duck').hatch(<f-args>)", { nargs = '?', complete = "customlist,v:lua.Eggs" })
+vim.api.nvim_create_user_command("HatchKill", ":lua KillDucks(<f-args>)", { nargs = '?' })
