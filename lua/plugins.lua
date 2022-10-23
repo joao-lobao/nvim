26691573ea518a6b300cef575e83e2915475a70b
joao <joao_loba@sapo.pt>
2022-10-23 05:44:00 +0000

    better organization and commenting on plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,27 +1,34 @@
-vim.cmd([[
+-- auto install packer if not installed
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer() -- true if packer was just installed
+
+-- autocommand that reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[ 
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
   augroup end
 ]])
 
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+-- import packer safely
+local status, packer = pcall(require, "packer")
+if not status then
+	return
 end
 
-local packer_bootstrap = ensure_packer()
-
--- Install your plugins here
-return require('packer').startup(function(use)
-  -- My plugins here
-
+-- add list of plugins to install
+return packer.startup(function(use)
   -- Have packer manage itself
   use({ 'wbthomason/packer.nvim', opt = true }) 
   -- speed loading Lua modules and files.
