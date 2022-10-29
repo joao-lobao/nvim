33be7d5e40094e677f5fd5a6980aefd443e2bff5
joao <joao_loba@sapo.pt>
2022-10-29 04:33:31 +0000

    adds devicons to startify

--- lua/user/startify.lua
+++ lua/user/startify.lua
@@ -2,6 +2,20 @@ vim.api.nvim_set_keymap('n', '<leader>S', ':Startify<CR>', { noremap = true })
 
 vim.g.startify_session_dir = "~/.config/nvim/session"
 
+-- global function to replace vim-devicons for nvim-web-devicons
+-- replace function
+function _G.webDevIcons(path)
+  local filename = vim.fn.fnamemodify(path, ':t')
+  local extension = vim.fn.fnamemodify(path, ':e')
+  return require'nvim-web-devicons'.get_icon(filename, extension, { default = true })
+end
+-- function to be replaced
+vim.cmd[[
+function! StartifyEntryFormat() abort
+  return 'v:lua.webDevIcons(absolute_path) . " " . entry_path'
+endfunction
+]]
+
 vim.g.startify_lists = {
   { type= 'sessions',  header= { '   Sessions' }},
   { type= 'bookmarks', header= { '   Bookmarks' }},
