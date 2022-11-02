61f5a60b0ab2390cf717a405df1ab887785959ab
joao <joao_loba@sapo.pt>
2022-11-02 04:48:04 -0100

    indent startify file

--- lua/user/startify.lua
+++ lua/user/startify.lua
@@ -11,17 +11,17 @@ function _G.webDevIcons(path)
 end
 -- function to be replaced
 vim.cmd[[
-function! StartifyEntryFormat() abort
-  return 'v:lua.webDevIcons(absolute_path) . " " . entry_path'
-endfunction
+  function! StartifyEntryFormat() abort
+    return 'v:lua.webDevIcons(absolute_path) . " " . entry_path'
+  endfunction
 
-" customize Startify text colors
-autocmd ColorScheme gruvbox highlight StartifyNumber  guifg=#85DACD
-autocmd ColorScheme gruvbox highlight StartifyBracket guifg=gray
-autocmd ColorScheme gruvbox highlight StartifySpecial guifg=#C4CCFF
-autocmd ColorScheme gruvbox highlight StartifySection guifg=#CB7080
-autocmd ColorScheme gruvbox highlight StartifyPath    guifg=orange
-autocmd ColorScheme gruvbox highlight StartifyFile    guifg=#97FF97
+  " customize Startify text colors
+  autocmd ColorScheme gruvbox highlight StartifyNumber  guifg=#85DACD
+  autocmd ColorScheme gruvbox highlight StartifyBracket guifg=gray
+  autocmd ColorScheme gruvbox highlight StartifySpecial guifg=#C4CCFF
+  autocmd ColorScheme gruvbox highlight StartifySection guifg=#CB7080
+  autocmd ColorScheme gruvbox highlight StartifyPath    guifg=orange
+  autocmd ColorScheme gruvbox highlight StartifyFile    guifg=#97FF97
 ]]
 
 vim.g.startify_lists = {
