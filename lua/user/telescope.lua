9f8327b9a54ed8fef0de3924034bdb79ef039aab
joao <joao_loba@sapo.pt>
2022-11-03 12:28:42 -0100

    better telescope find files

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -16,17 +16,18 @@ require('telescope').setup {
 local M = require('telescope.builtin')
 function M.search_files_in_home()
   local opts = {}
+  opts.prompt_title= "Search files in home"
   opts.cwd = "~"
   opts.hidden = true
   opts.no_ignore = true
-  require('telescope.builtin').find_files(opts)
+  M.find_files(opts)
 end
 
 
 -- find git files
 vim.api.nvim_set_keymap('n', '<leader>gF', '<Cmd>Telescope git_files<CR>', { noremap = true, silent = true })
 -- find all files (doesn't respect .gitignore)
-vim.api.nvim_set_keymap('n', '<leader>F', '<Cmd>Telescope find_files no_ignore=true<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>F', '<Cmd>Telescope find_files no_ignore=true hidden=true<CR>', { noremap = true, silent = true })
 -- grep respecting .gitignore
 vim.api.nvim_set_keymap('n', '<leader>r', '<Cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>y', '<Cmd>Telescope registers<CR>', { noremap = true, silent = true })
