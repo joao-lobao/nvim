7d41bea69dccd9200e709b42b4843bee9d0257e6
joao <joao_loba@sapo.pt>
2022-11-04 14:39:34 -0100

    adds git stage hunk keymap

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -6,6 +6,7 @@ end
 
 -- configure/enable gitsigns
 gitsigns.setup()
+vim.api.nvim_set_keymap('n', '<leader>gs', ':Gitsigns stage_hunk<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>gu', ':Gitsigns reset_hunk<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>gp', ':Gitsigns prev_hunk<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>gn', ':Gitsigns next_hunk<CR>', { noremap = true })
