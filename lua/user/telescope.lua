7456fdbc1f6f428cda4ea1a1e32601b5e78c3564
joao <joao_loba@sapo.pt>
2022-11-06 05:17:11 -0100

    telescope lint fixing and livegrep customization

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -1,47 +1,58 @@
-local actions = require('telescope.actions')
-local telescope = require('telescope')
+local actions = require("telescope.actions")
+local telescope = require("telescope")
 
-telescope.setup {
-  defaults = {
-    prompt_prefix = " 🔍 ",
-    layout_config = {prompt_position = "top"},
-    sorting_strategy = "ascending",
-    mappings = {
-      n = {
-        ["q"] = actions.close
-      }
-    }
-  }
-}
+telescope.setup({
+	defaults = {
+		prompt_prefix = " 🔍 ",
+		layout_config = { prompt_position = "top" },
+		sorting_strategy = "ascending",
+		mappings = {
+			n = {
+				["q"] = actions.close,
+			},
+		},
+	},
+})
 -- load refactoring Telescope extension
 telescope.load_extension("refactoring")
 
-local M = require('telescope.builtin')
+local M = require("telescope.builtin")
 function M.search_files_in_home()
-  local opts = {}
-  opts.prompt_title= "Search files in home"
-  opts.cwd = "~"
-  opts.hidden = true
-  opts.no_ignore = true
-  M.find_files(opts)
+	local opts = {}
+	opts.prompt_title = "Search files in home"
+	opts.cwd = "~"
+	opts.hidden = true
+	opts.no_ignore = true
+	M.find_files(opts)
 end
 
 
+
+local opts = { noremap = true, silent = true }
+
 -- find git files
-vim.api.nvim_set_keymap('n', '<leader>gF', '<Cmd>Telescope git_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>gF", "<Cmd>Telescope git_files<CR>", opts)
 -- find all files (doesn't respect .gitignore)
-vim.api.nvim_set_keymap('n', '<leader>F', '<Cmd>Telescope find_files no_ignore=true hidden=true<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<leader>F", "<Cmd>Telescope find_files no_ignore=true hidden=true<CR>", opts)
 -- grep respecting .gitignore
-vim.api.nvim_set_keymap('n', '<leader>r', '<Cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>y', '<Cmd>Telescope registers<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>r",
+	':lua require("telescope.builtin").live_grep({ additional_args = function(opts) return { "--hidden" } end })<CR>',
+	opts
+)
+vim.api.nvim_set_keymap("n", "<leader>y", "<Cmd>Telescope registers<CR>", opts)
 -- grep not respecting .gitignore
-vim.api.nvim_set_keymap('n', '<leader>R', ':lua require("telescope.builtin").live_grep({ additional_args = function(opts) return { "--no-ignore" } end })<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>R",
+	':lua require("telescope.builtin").live_grep({ additional_args = function(opts) return { "--no-ignore", "--hidden" } end })<CR>',
+	opts
+)
 -- find files from home dir as cwd
-vim.api.nvim_set_keymap('n', '<leader>~', '<Cmd>Telescope search_files_in_home<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>~", "<Cmd>Telescope search_files_in_home<CR>", opts)
 
-
-vim.api.nvim_set_keymap('n', '<leader>B', '<Cmd>Telescope buffers<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>M', '<Cmd>Telescope marks<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>B", "<Cmd>Telescope buffers<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>M", "<Cmd>Telescope marks<CR>", opts)
 -- find in current buffer
-vim.api.nvim_set_keymap('n', '<leader>/', '<Cmd>Telescope current_buffer_fuzzy_find<CR>', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap("n", "<leader>/", "<Cmd>Telescope current_buffer_fuzzy_find<CR>", opts)
