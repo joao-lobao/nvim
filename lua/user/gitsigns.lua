4641ed30e70a35cc6fca1af2f69215badfec37bb
joao <joao_loba@sapo.pt>
2022-11-06 10:21:55 -0100

    adds keymap to preview git hunk change

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -10,3 +10,4 @@ vim.api.nvim_set_keymap('n', '<leader>gs', ':Gitsigns stage_hunk<CR>', { noremap
 vim.api.nvim_set_keymap('n', '<leader>gu', ':Gitsigns reset_hunk<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>gp', ':Gitsigns prev_hunk<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>gn', ':Gitsigns next_hunk<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>gk', ':Gitsigns preview_hunk_inline<CR>', { noremap = true })
