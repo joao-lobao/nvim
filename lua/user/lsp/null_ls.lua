14cda3ff41a58e475d66d3c331a745d290ad0344
joao <joao_loba@sapo.pt>
2022-11-09 05:19:55 -0100

    adds format on save

--- lua/user/lsp/null_ls.lua
+++ lua/user/lsp/null_ls.lua
@@ -7,6 +7,7 @@ end
 -- for conciseness
 local formatting = null_ls.builtins.formatting -- to setup formatters
 local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 -- configure null_ls
 null_ls.setup({
@@ -19,4 +20,16 @@ null_ls.setup({
 		diagnostics.markdownlint,
 		diagnostics.eslint_d,
 	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
