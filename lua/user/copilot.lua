9af74e3e6b72bb40f734edf241bf7757b0020ff8
joao <joao_loba@sapo.pt>
2022-12-17 23:51:16 -0100

    fixes copilot keymaps
    fixes copilot keymaps

--- lua/user/copilot.lua
+++ lua/user/copilot.lua
@@ -1,4 +1,6 @@
 vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<C-j>", '<Plug>(copilot-next)', { silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<C-k>", '<Plug>(copilot-previous)', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<c-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<c-j>", "<Plug>(copilot-next)", { noremap = false })
+vim.api.nvim_set_keymap("i", "<c-k>", "<Plug>(copilot-previous)", { noremap = false })
+vim.api.nvim_set_keymap("i", "<c-e>", "<Plug>(copilot-dismiss)", { noremap = false })
+vim.api.nvim_set_keymap("i", "<c-s>", "<Plug>(copilot-suggest)", { noremap = false })
