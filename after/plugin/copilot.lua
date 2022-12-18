ad5c3cbaecaec81e5764a9e4d311c4675c2c2498
joao <joao_loba@sapo.pt>
2022-12-18 04:48:45 -0100

    another fix for copilot keymaps

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,6 +1,6 @@
 vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<c-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<c-j>", "<Plug>(copilot-next)", { noremap = false })
-vim.api.nvim_set_keymap("i", "<c-k>", "<Plug>(copilot-previous)", { noremap = false })
-vim.api.nvim_set_keymap("i", "<c-e>", "<Plug>(copilot-dismiss)", { noremap = false })
-vim.api.nvim_set_keymap("i", "<c-s>", "<Plug>(copilot-suggest)", { noremap = false })
+vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<M-j>", "<Plug>(copilot-next)", { noremap = false })
+vim.api.nvim_set_keymap("i", "<M-k>", "<Plug>(copilot-previous)", { noremap = false })
+vim.api.nvim_set_keymap("i", "<C-e>", "<Plug>(copilot-dismiss)", { noremap = false })
+vim.api.nvim_set_keymap("i", "<C-s>", "<Plug>(copilot-suggest)", { noremap = false })
