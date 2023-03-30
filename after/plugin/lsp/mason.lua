1acb19f6ca067393a9af0c0bcb56461d39cd4e60
joao <joao_loba@sapo.pt>
2023-03-30 16:45:05 +0000

    replaces sumneko_lua for lua_ls

--- after/plugin/lsp/mason.lua
+++ after/plugin/lsp/mason.lua
@@ -25,7 +25,7 @@ mason_lspconfig.setup({
 		"tsserver",
 		"html",
 		"cssls",
-		"sumneko_lua",
+		"lua_ls",
 	},
 	-- auto-install configured servers (with lspconfig)
 	automatic_installation = true, -- not the same as ensure_installed
