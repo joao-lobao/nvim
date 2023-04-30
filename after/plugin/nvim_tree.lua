6dbb17b15bd3ba096bcfa6533b57e4e73d436257
Joao Lobao <joao_lobao@sapo.pt>
2023-04-30 01:26:51 +0000

    changes deprecated configuration for nvim-tree

--- after/plugin/nvim_tree.lua
+++ after/plugin/nvim_tree.lua
@@ -1,15 +1,20 @@
+local function my_on_attach(bufnr)
+	local api = require("nvim-tree.api")
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+  api.config.mappings.default_on_attach(bufnr)
+	vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+	vim.keymap.set("n", "q", api.tree.close, opts("Close"))
+end
+
 require("nvim-tree").setup({
+	on_attach = my_on_attach,
 	update_focused_file = {
 		enable = true,
 		update_root = true,
-	},
-	view = {
-		mappings = {
-			list = {
-				{ key = { "l" }, action = "edit" },
-				{ key = "h", action = "close_node" },
-			},
-		},
 	},
 	actions = {
 		open_file = {
