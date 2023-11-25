e8338efc6d0835157404b7923935c8f5b8d3daf6
Joao Lobao <joao_lobao@sapo.pt>
2023-11-25 10:46:40 -0100

    adds session name on lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,3 +1,13 @@
+local custom_fname = require("lualine.components.filename"):extend()
+
+function custom_fname:update_status()
+	local session = vim.fn.fnamemodify(vim.v.this_session, ":t")
+  if session == "" then
+    return "No Session"
+  end
+	return "Session: " .. session
+end
+
 require("lualine").setup({
 	options = {
 		-- theme = "gruvbox",
@@ -11,5 +21,6 @@ require("lualine").setup({
 				show_modified_status = true, -- Shows indicator when the buffer is modified.
 			},
 		},
+		lualine_z = { custom_fname },
 	},
 })
