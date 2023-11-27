ec93cab15ae668b601bd84e7520a1cb1b70ed7fb
Joao Lobao <joao_lobao@sapo.pt>
2023-11-27 06:06:32 -0100

    adds customized snippets for luasnip

--- lua/user/utils/init.lua
+++ lua/user/utils/init.lua
@@ -1,4 +1,5 @@
 require("user.utils.general")
+require("user.utils.luasnip")
 require("user.utils.nvim_cmp")
 require("user.utils.nvim_telescope")
 require("user.utils.session-manager")
---
+++ lua/user/utils/luasnip.lua
@@ -0,0 +1,35 @@
+-- import luasnip plugin safely
+local luasnip_status, luasnip = pcall(require, "luasnip")
+if not luasnip_status then
+	return
+end
+
+local snip = luasnip.snippet
+local text = luasnip.text_node
+local insert = luasnip.insert_node
+local func = luasnip.function_node
+local repeater = require("luasnip.extras").rep
+
+local date = function()
+	return { os.date("%d/%m/%Y") }
+end
+
+luasnip.add_snippets(nil, {
+	all = {
+		snip({
+			trig = "meta",
+			name = "Metadata",
+			desc = "Comments/notes metadata ",
+		}, {
+			text({ "---", "title: " }),
+			insert(1, "note_title"),
+			text({ "", "date created: " }),
+			func(date, {}),
+			text({ "", "tags: [" }),
+			repeater(1),
+			insert(2),
+			text({ "]" }),
+			text({ "", "---" }),
+		}),
+	},
+})
