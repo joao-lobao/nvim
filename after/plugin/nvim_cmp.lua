a261daf09305806b8b312fc9b52728d589acc0fe
Joao Lobao <joao_lobao@sapo.pt>
2023-11-28 12:37:54 -0100

    adds Tab as cmp items selector

--- after/plugin/nvim_cmp.lua
+++ after/plugin/nvim_cmp.lua
@@ -29,6 +29,8 @@ cmp.setup({
 		end,
 	},
 	mapping = cmp.mapping.preset.insert({
+		["<Tab>"] = cmp.mapping.select_next_item(), -- navigate to next item
+    ["<S-Tab>"] = cmp.mapping.select_prev_item(), -- navigate to previous item
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
