cbd1c675b35d0e4896d4bdc22f00817854be42fa
Joao Lobao <joao_lobao@sapo.pt>
2023-11-30 10:08:33 -0100

    changes mapping for Telescope buffers cmd

--- after/plugin/nvim_telescope.lua
+++ after/plugin/nvim_telescope.lua
@@ -8,7 +8,7 @@ telescope.setup({
 		sorting_strategy = "ascending",
 		mappings = {
 			i = {
-				["Ç"] = actions.close,
+				["<Del>"] = actions.close,
 			},
 			n = {
 				["q"] = actions.close,
@@ -67,7 +67,7 @@ vim.api.nvim_set_keymap("n", "<leader>m", "<Cmd>Telescope common_actions<CR>", {
 
 vim.api.nvim_set_keymap("n", "<leader>y", "<Cmd>Telescope registers<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>B", "<Cmd>Telescope buffers<CR>", opts)
-vim.api.nvim_set_keymap("n", "Ç", "<Cmd>Telescope buffers<CR>", opts)
+vim.api.nvim_set_keymap("n", "<Enter>", "<Cmd>Telescope buffers<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>M", "<Cmd>Telescope marks<CR>", opts)
 -- find in current buffer
 vim.api.nvim_set_keymap("n", "<leader>/", "<Cmd>Telescope current_buffer_fuzzy_find<CR>", opts)
