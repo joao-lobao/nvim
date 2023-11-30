475c18cb5a57e64b33511613c48d34fc75d06fc3
Joao Lobao <joao_lobao@sapo.pt>
2023-11-30 15:55:05 -0100

    changes colorscheme for custom statusline

--- lua/user/utils/custom_statusline.lua
+++ lua/user/utils/custom_statusline.lua
@@ -1,11 +1,20 @@
 --statusline highlights
-vim.api.nvim_set_hl(0, "StatusType", { bg = "#458588", fg = "#1d2021" })
-vim.api.nvim_set_hl(0, "StatusFile", { bg = "#98971a", fg = "#1d2021" })
-vim.api.nvim_set_hl(0, "StatusModified", { bg = "teal", fg = "white" })
-vim.api.nvim_set_hl(0, "StatusBuffer", { bg = "#98971a", fg = "#1d2021" })
-vim.api.nvim_set_hl(0, "StatusLocation", { bg = "#458588", fg = "#1d2021" })
-vim.api.nvim_set_hl(0, "StatusPercent", { bg = "#1d2021", fg = "#ebdbb2" })
-vim.api.nvim_set_hl(0, "StatusNorm", { bg = "none", fg = "white" })
+local dark_gray = "#282a36"
+local light_purple = "#bd93f9"
+local dark_purple = "#6272a4"
+local cyan = "#8be9fd"
+local green = "#50fa7b"
+local orange = "#ffb86c"
+local pink = "#ff79c6"
+
+vim.api.nvim_set_hl(0, "StatusType", { bg = dark_gray, fg = dark_purple })
+vim.api.nvim_set_hl(0, "StatusFile", { bg = dark_gray, fg = light_purple })
+vim.api.nvim_set_hl(0, "StatusModified", { bg = green, fg = dark_gray })
+vim.api.nvim_set_hl(0, "StatusNorm", { bg = dark_gray, fg = green })
+vim.api.nvim_set_hl(0, "StatusEncoding", { bg = dark_gray, fg = pink })
+vim.api.nvim_set_hl(0, "StatusBuffer", { bg = dark_gray, fg = orange })
+vim.api.nvim_set_hl(0, "StatusLocation", { bg = dark_gray, fg = dark_purple })
+vim.api.nvim_set_hl(0, "StatusPercent", { bg = dark_gray, fg = cyan })
 
 -- statusline
 vim.o.statusline = "%#StatusType#"
@@ -19,10 +28,10 @@ vim.o.statusline = "%#StatusType#"
 	.. "%#StatusModified#"
 	.. "%m"
 	.. "%#StatusNorm#"
-	.. " "
-	.. "%{fugitive#statusline()}"
-	.. "%#StatusNorm#"
 	.. "%="
+	.. "%{fugitive#statusline()}"
+	.. "%#StatusEncoding#"
+	.. " "
 	.. "%{&fileencoding?&fileencoding:&encoding}"
 	.. " "
 	.. "%#StatusBuffer#"
