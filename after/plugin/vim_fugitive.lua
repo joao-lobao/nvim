885cd4f5f8472f9d052f4147f80026131d84c823
Joao Lobao <joao_lobao@sapo.pt>
2023-12-01 14:36:54 -0100

    a better vim_fugitive goto Git status

--- after/plugin/vim_fugitive.lua
+++ after/plugin/vim_fugitive.lua
@@ -9,3 +9,7 @@ vim.api.nvim_set_keymap("n", "<leader>gh", ":diffget //2<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>gl", ":diffget //3<CR>", opts)
 
 vim.api.nvim_set_keymap("n", "gb", ":Git blame<CR>", opts)
+
+-- use fugitive autocmds to a better goto Git status
+vim.cmd([[:autocmd User FugitiveIndex silent! /un]])
+vim.cmd([[:autocmd User FugitiveChanged silent! /un]])
