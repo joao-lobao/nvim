e7d7798d297be09a270667481bd0e305e1687d75
Joao Lobao <joao_lobao@sapo.pt>
2023-12-01 21:23:17 -0100

    fixes previous custom_goto refactor

--- lua/user/utils/custom_goto.lua
+++ lua/user/utils/custom_goto.lua
@@ -1,6 +1,4 @@
-local M = {}
-
-M.get_current = function(obj_type, obj_identifier, word_index)
+local get_current = function(obj_type, obj_identifier, word_index)
 	local ts_utils = require("nvim-treesitter.ts_utils")
 	local expr = ts_utils.get_node_at_cursor()
 
@@ -31,23 +29,21 @@ M.get_current = function(obj_type, obj_identifier, word_index)
 end
 
 -- goto class and method definitions
-local custom_goto = require("user.utils.custom_goto")
 vim.api.nvim_create_user_command("GotoClass", function()
   -- typescript class
-	custom_goto.get_current("class_declaration", "type_identifier", 1)
+	get_current("class_declaration", "type_identifier", 1)
   -- javascript class
-	custom_goto.get_current("class_declaration", "identifier", 1)
+	get_current("class_declaration", "identifier", 1)
 end, {})
 
 vim.api.nvim_create_user_command("GotoFunction", function()
   -- typescript and javascript class method
-	custom_goto.get_current("method_definition", "property_identifier", 0)
+	get_current("method_definition", "property_identifier", 0)
   -- typescript and javascript function
-	custom_goto.get_current("function_declaration", "identifier", 1)
+	get_current("function_declaration", "identifier", 1)
 end, {})
 -- custom goto mappings
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "gtm", ":GotoFunction<CR>", opts)
 vim.api.nvim_set_keymap("n", "gtc", ":GotoClass<CR>", opts)
 
-return M
--- lua/user/utils/init.lua
+++ lua/user/utils/init.lua
@@ -1,4 +1,5 @@
 require("user.utils.general")
+require("user.utils.custom_goto")
 require("user.utils.custom_statusline")
 require("user.utils.nvim_telescope")
 require("user.utils.session-manager")
