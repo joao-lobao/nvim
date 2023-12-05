0c54d9020b7405eef1f124cc646e6f1dcc891010
Joao Lobao <joao_lobao@sapo.pt>
2023-12-05 18:06:02 -0100

    creates custom gitgutter and stage/reset hunks functionality

---
+++ lua/user/utils/custom_git.lua
@@ -0,0 +1,85 @@
+-- stage/reset hunks
+local opts = { noremap = true, silent = true }
+Reset_or_Add_Hunk = function(action)
+	local buffer_lines = vim.fn.line("$")
+	local cursor_line = vim.fn.line(".")
+
+	vim.api.nvim_command("Gdiffsplit")
+	if action == "reset" then
+		if cursor_line == buffer_lines then
+			vim.api.nvim_command("wincmd l")
+			vim.api.nvim_command("normal! j")
+			vim.api.nvim_command("diffget")
+			vim.api.nvim_command("w")
+			vim.api.nvim_command("wincmd h")
+		end
+	else
+		vim.api.nvim_command("diffget")
+		vim.api.nvim_command("w")
+	end
+	vim.api.nvim_command("q")
+end
+vim.api.nvim_set_keymap("n", "<leader>gu", "<cmd>lua Reset_or_Add_Hunk('reset')<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>lua Reset_or_Add_Hunk('stage')<CR>", opts)
+
+-- Custom Git Gutter
+-- define signs
+vim.api.nvim_command("sign define diffchange text=| texthl=GruvboxAquaBold")
+vim.api.nvim_command("sign define diffadd text=| texthl=GruvboxGreenBold")
+vim.api.nvim_command("sign define diffdelete text=| texthl=GruvboxRedBold")
+
+local get_type = function(line)
+	if vim.startswith(line, "+") then
+		return "diffadd"
+	elseif vim.startswith(line, "-") then
+		return "diffdelete"
+	else
+		return "diffchange"
+	end
+end
+
+local sign_line = function(line_number, line)
+	vim.api.nvim_command("sign place 1 line=" .. line_number .. " name=" .. get_type(line))
+end
+
+-- check buffer is a file in a git project
+local is_file_inside_a_git_project = function()
+	local file_dir = vim.fn.expand("%:p:h")
+	return vim.fn.system("git -C " .. file_dir .. " rev-parse --is-inside-work-tree") == "true\n"
+end
+
+SetDiffSigns = function()
+	if not is_file_inside_a_git_project() then
+		return
+	end
+
+	vim.api.nvim_command("call sign_unplace('')")
+	local path = vim.fn.expand("%:p")
+	local diff = vim.fn.systemlist("git diff --unified=0 " .. path)
+
+	-- iterate over changed hunks
+	for index, line in ipairs(diff) do
+		if vim.startswith(line, "@@") then
+			local cline_nlines_pair = string.sub(vim.split(line, " ")[3], 2)
+			local line_number = vim.split(cline_nlines_pair, ",")[1]
+			local nr_of_lines = vim.split(cline_nlines_pair, ",")[2]
+
+			-- check change is one line only
+			if nr_of_lines == nil or nr_of_lines == "0" then
+				sign_line(line_number, diff[index + 1])
+			else
+				-- sign when multiple lines are changed
+				for i = 1, nr_of_lines do
+					sign_line(line_number + i - 1, diff[index + 1])
+				end
+			end
+		end
+	end
+end
+
+-- autocommands
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+	callback = function()
+		SetDiffSigns()
+	end,
+})
