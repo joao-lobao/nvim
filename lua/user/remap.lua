60d1d030b00e1df242921ed3cc86061e0e99773d
Joao Lobao <joao_lobao@sapo.pt>
2023-12-10 20:36:10 -0100

    adds mapping to calculate math expressions

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -83,3 +83,5 @@ vim.api.nvim_set_keymap("i", '"', '""<left>', opts)
 vim.api.nvim_set_keymap("i", "(", "()<left>", opts)
 vim.api.nvim_set_keymap("i", "[", "[]<left>", opts)
 vim.api.nvim_set_keymap("i", "{", "{}<left>", opts)
+-- calculate math expression
+vim.api.nvim_set_keymap("n", "==", "0yt=A<C-r>=<C-r>\"<CR><Esc>", opts)
