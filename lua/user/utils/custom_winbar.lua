ce0a92b0b6d13ad5020356881019ca21252a9217
Joao Lobao <joao_lobao@sapo.pt>
2023-12-15 18:50:52 -0100

    adds vim session info

--- lua/user/utils/custom_winbar.lua
+++ lua/user/utils/custom_winbar.lua
@@ -1,3 +1,4 @@
+-- other possible color combinations on winbar with white, light_purple, dark_purple
 List_opened_buffers = function()
 	local buffers = vim.fn.getbufinfo({ buflisted = 1 })
 	local current_buffer_path = vim.fn.expand("%:p")
@@ -11,36 +12,41 @@ List_opened_buffers = function()
 
 			-- buffer name is current buffer
 			if buffer_path == current_buffer_path then
-				table.insert(buffer_names, "%#StatusTypeInverseLight#" .. " %t" .. "%M " .. "%#StatusType#")
+				table.insert(buffer_names, "%#StatusFile#󰉺" .. " %t" .. "%M " .. "%#StatusType#")
 			else
 				table.insert(buffer_names, " " .. buffer_name .. is_modified)
 			end
 		end
 	end
-	return "%#StatusType#" .. table.concat(buffer_names, " ") .. "%="
+	return "%#StatusType# " .. table.concat(buffer_names, " ") .. "%="
 end
 
-local dark_purple = "#6272a4"
-vim.api.nvim_set_hl(0, "StatusTypeInverseLight", { bg = dark_purple })
-
 Get_git_info = function()
-	local current_cwd = " " .. vim.fn.getcwd() .. " "
 	local file_dir = vim.fn.expand("%:p:h")
 	local is_file_in_git_project = vim.fn.system("git -C " .. file_dir .. " rev-parse --is-inside-work-tree")
 		== "true\n"
 	local git_info = " No git repo "
 	if is_file_in_git_project then
 		git_info = " " .. string.sub(vim.fn.system("git -C " .. file_dir .. " show -s --format=%s"), 0, 50) .. " "
-    git_info = string.gsub(git_info, "\n", "")
+		git_info = string.gsub(git_info, "\n", "")
 	end
-	return "%#StatusNorm#" .. git_info .. "%#StatusType# " .. current_cwd
+	return "%#StatusNorm#" .. git_info
+end
+
+Get_cwd_info = function()
+	local session = vim.fn.fnamemodify(vim.v.this_session, ":t")
+	if session ~= "" then
+		session = " " .. session .. " "
+	end
+	local current_cwd = " " .. vim.fn.getcwd() .. " "
+	return "%#StatusEncoding#" .. session .. "%#StatusBuffer#" .. current_cwd
 end
 
 -- on BufEnter update winbar
 local group = vim.api.nvim_create_augroup("CustomWinBar", { clear = true })
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
 	callback = function()
-		vim.cmd("lua vim.o.winbar = List_opened_buffers() .. Get_git_info()")
+		vim.cmd("lua vim.o.winbar = List_opened_buffers() .. Get_git_info() .. Get_cwd_info()")
 	end,
 	group = group,
 })
