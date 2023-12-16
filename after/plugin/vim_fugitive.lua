968bf3e1e07265e780de444ee110d918fd35d089
Joao Lobao <joao_lobao@sapo.pt>
2023-12-16 09:15:37 -0100

    cleans up vim-fugitive config

--- after/plugin/vim_fugitive.lua
+++ after/plugin/vim_fugitive.lua
@@ -1,19 +1,13 @@
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "gs", ":G<CR>", opts)
-
 vim.api.nvim_set_keymap("n", "gl", ":Gclog<CR><C-w>j", opts)
-vim.api.nvim_set_keymap("n", "g%", ":Gclog -- %<CR><C-w>j", opts)
-
-vim.api.nvim_set_keymap("n", "<leader>gd", ":Gdiffsplit<CR>", opts)
+vim.api.nvim_set_keymap("n", "gb", ":Git blame<CR>", opts)
+vim.api.nvim_set_keymap("n", "gp", "[c", opts)
+vim.api.nvim_set_keymap("n", "gn", "]c", opts)
 vim.api.nvim_set_keymap("n", "<leader>gh", ":diffget //2<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>gl", ":diffget //3<CR>", opts)
-
-vim.api.nvim_set_keymap("n", "gb", ":Git blame<CR>", opts)
-
--- get last commit message
 vim.api.nvim_create_user_command("GitLastCommit", "Git show -1", {})
 
--- hunks navigation
 ToggleDiffView = function()
 	if vim.o.diff == false then
 		vim.api.nvim_command("Gdiffsplit")
@@ -21,8 +15,4 @@ ToggleDiffView = function()
 		vim.api.nvim_command("q")
 	end
 end
-
 vim.api.nvim_set_keymap("n", "vd", "<cmd>lua ToggleDiffView()<CR>", opts)
--- goto previous and next hunk
-vim.api.nvim_set_keymap("n", "<leader>gp", "[c", opts)
-vim.api.nvim_set_keymap("n", "<leader>gn", "]c", opts)
