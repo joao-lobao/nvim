4d187586a8aa44665133683ebe394d5c5248e0fb
Joao Lobao <joao_lobao@sapo.pt>
2023-12-17 05:37:24 -0100

    fixes empty buffer showing on buffers list

--- lua/user/utils/custom_winbar.lua
+++ lua/user/utils/custom_winbar.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_set_hl(0, "StatusTypeLight", { bg = Dark_gray, fg = Gray})
+vim.api.nvim_set_hl(0, "StatusTypeLight", { bg = Dark_gray, fg = Gray })
 vim.api.nvim_set_hl(0, "StatusFileInverseLight", { bg = Dark_purple, fg = White })
 
 Buffers = function()
@@ -8,17 +8,19 @@ Buffers = function()
 	for _, buffer in ipairs(buffers) do
 		local buffer_path = buffer.name
 		local buffer_name = vim.fn.fnamemodify(buffer_path, ":t")
-		-- buffer name is different from empty string
-		if buffer_name == "" then
+		-- buffer name is different from empty string and is selected
+		if buffer_name == "" and buffer_path == current_buffer_path then
 			buffer_name = "[No Name]"
 		end
 		local is_modified = buffer.changed == 1 and " + " or " "
 
-		-- buffer name is current buffer
-		if buffer_path == current_buffer_path then
-			table.insert(buffer_names, "%#StatusFileInverseLight#" .. " %t" .. "%M " .. "%#StatusTypeLight#")
-		else
-			table.insert(buffer_names, " " .. buffer_name .. is_modified)
+		if buffer_name ~= "" then
+			-- buffer name is current buffer
+			if buffer_path == current_buffer_path then
+				table.insert(buffer_names, "%#StatusFileInverseLight#" .. " %t" .. "%M " .. "%#StatusTypeLight#")
+			else
+				table.insert(buffer_names, " " .. buffer_name .. is_modified)
+			end
 		end
 	end
 	return "%#StatusTypeLight#" .. table.concat(buffer_names, " ")
