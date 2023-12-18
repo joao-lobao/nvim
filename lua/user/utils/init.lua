583dbc918e56c9d5a65420bb3014498df26b499d
Joao Lobao <joao_lobao@sapo.pt>
2023-12-18 00:29:21 -0100

    almost full functionality only missing custom null_ls features; commented the minimal config features
    Revert "commented features"
    
    This reverts commit a043d9c7a32e21eb9b68e84c0dc5bf6732dcfc8c.
    
    almost full functionality for custom features

--- lua/user/utils/init.lua
+++ lua/user/utils/init.lua
@@ -16,3 +16,21 @@ require("user.utils.autocommands")
 -- require("user.utils.custom_statusline") statusline can be improved with nvim_eval_statusline
 -- require("user.utils.custom_winbar") winbar can be improved by using WinBarNC and nvim_eval_statusline
 -- require("user.utils.netrw") netrw can also be disabled by setting g:loaded_netrwPlugin to 1 and g:loaded_netrw to 1
+-- custom null_ls/none-ls feature and extra_args = get_eslint_rules() which is responsible for long startup time
+
+-- netrw
+-- vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- statusline and winbar
+-- local dark_gray = "#282a36"
+-- local green = "#50fa7b"
+-- local red = "#ff5555"
+-- local light_purple = "#bd93f9"
+-- local bright_orange = "#fe8019"
+-- vim.api.nvim_set_hl(0, "MsgArea", { bg = dark_gray, fg = green })
+-- vim.api.nvim_set_hl(0, "ErrorMsg", { bg = dark_gray, fg = red })
+-- vim.api.nvim_set_hl(0, "StatusLine", { bg = dark_gray, fg = bright_orange })
+-- vim.api.nvim_set_hl(0, "WinBar", { bg = dark_gray, fg = light_purple })
+-- vim.api.nvim_set_hl(0, "CustomMod", { bg = green, fg = dark_gray })
+-- winbar
+-- vim.o.winbar = "%=%#StatusLine# %y %#Winbar#%F %#CustomMod#%m%*%="
