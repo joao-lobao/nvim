7682323e314982b0796541327ab874527f40a609
Joao Lobao <joao_lobao@sapo.pt>
2023-12-18 01:07:15 -0100

    full functionality commented; only missing the custom null_ls feature uncommented simple functionality on utils/init.lua fugitive has replaced remaps for gp and gn

--- after/plugin/vim_fugitive.lua
+++ after/plugin/vim_fugitive.lua
@@ -6,6 +6,8 @@ vim.api.nvim_set_keymap("n", "gb", ":Git blame<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>gh", ":diffget //2<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>gl", ":diffget //3<CR>", opts)
 vim.api.nvim_create_user_command("GitLastCommit", "Git show -1", {})
+vim.api.nvim_set_keymap("n", "gp", "[c", opts)
+vim.api.nvim_set_keymap("n", "gn", "]c", opts)
 
 ToggleDiffView = function()
 	if vim.o.diff == false then
@@ -16,59 +18,59 @@ ToggleDiffView = function()
 end
 vim.api.nvim_set_keymap("n", "vd", "<cmd>lua ToggleDiffView()<CR>", opts)
 
--- stage/reset hunks
-StageHunk = function()
-	local buffer_lines = vim.fn.line("$")
-	local cursor_line = vim.fn.line(".")
+-- -- stage/reset hunks
+-- StageHunk = function()
+-- 	local buffer_lines = vim.fn.line("$")
+-- 	local cursor_line = vim.fn.line(".")
 
-	vim.api.nvim_command("Gdiffsplit")
-	if cursor_line == buffer_lines then
-		vim.api.nvim_command("normal! j")
-	end
-	vim.api.nvim_command("diffget")
-	vim.api.nvim_command("w")
-	vim.api.nvim_command("q")
-end
+-- 	vim.api.nvim_command("Gdiffsplit")
+-- 	if cursor_line == buffer_lines then
+-- 		vim.api.nvim_command("normal! j")
+-- 	end
+-- 	vim.api.nvim_command("diffget")
+-- 	vim.api.nvim_command("w")
+-- 	vim.api.nvim_command("q")
+-- end
 
-ResetHunk = function()
-	local buffer_lines = vim.fn.line("$")
-	local cursor_line = vim.fn.line(".")
+-- ResetHunk = function()
+-- 	local buffer_lines = vim.fn.line("$")
+-- 	local cursor_line = vim.fn.line(".")
 
-	vim.api.nvim_command("Gdiffsplit")
-	if cursor_line == buffer_lines then
-		vim.api.nvim_command("wincmd l")
-		vim.api.nvim_command("normal! j")
-		vim.api.nvim_command("diffget")
-		vim.api.nvim_command("w")
-		vim.api.nvim_command("wincmd h")
-	else
-		vim.api.nvim_command("diffput")
-		vim.api.nvim_command("w")
-	end
-	vim.api.nvim_command("q")
-end
-vim.api.nvim_set_keymap("n", "<leader>gu", "<cmd>lua ResetHunk()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>lua StageHunk()<CR>", opts)
+-- 	vim.api.nvim_command("Gdiffsplit")
+-- 	if cursor_line == buffer_lines then
+-- 		vim.api.nvim_command("wincmd l")
+-- 		vim.api.nvim_command("normal! j")
+-- 		vim.api.nvim_command("diffget")
+-- 		vim.api.nvim_command("w")
+-- 		vim.api.nvim_command("wincmd h")
+-- 	else
+-- 		vim.api.nvim_command("diffput")
+-- 		vim.api.nvim_command("w")
+-- 	end
+-- 	vim.api.nvim_command("q")
+-- end
+-- vim.api.nvim_set_keymap("n", "<leader>gu", "<cmd>lua ResetHunk()<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "<leader>gs", "<cmd>lua StageHunk()<CR>", opts)
 
--- goto previous and next hunk
-Goto_hunk = function(direction)
-	local path = vim.fn.expand("%:p")
-	local diff = vim.fn.systemlist("git diff --unified=0 " .. path)
-	local cursor_line = vim.fn.line(".")
-	for _, line in ipairs(diff) do
-	  -- iterate over changed hunks
-		if vim.startswith(line, "@@") then
-			local cline_nlines_pair = string.sub(vim.split(line, " ")[3], 2)
-			local line_number = tonumber(vim.split(cline_nlines_pair, ",")[1])
-			if line_number > cursor_line and direction == "down" then
-				vim.api.nvim_command("normal! " .. line_number .. "G")
-				break
-			end
-			if line_number < cursor_line and direction == "up" then
-				vim.api.nvim_command("normal! " .. line_number .. "G")
-			end
-		end
-	end
-end
-vim.api.nvim_set_keymap("n", "gp", "<cmd>lua Goto_hunk('up')<CR>", opts)
-vim.api.nvim_set_keymap("n", "gn", "<cmd>lua Goto_hunk('down')<CR>", opts)
+-- -- goto previous and next hunk
+-- Goto_hunk = function(direction)
+-- 	local path = vim.fn.expand("%:p")
+-- 	local diff = vim.fn.systemlist("git diff --unified=0 " .. path)
+-- 	local cursor_line = vim.fn.line(".")
+-- 	for _, line in ipairs(diff) do
+-- 	  -- iterate over changed hunks
+-- 		if vim.startswith(line, "@@") then
+-- 			local cline_nlines_pair = string.sub(vim.split(line, " ")[3], 2)
+-- 			local line_number = tonumber(vim.split(cline_nlines_pair, ",")[1])
+-- 			if line_number > cursor_line and direction == "down" then
+-- 				vim.api.nvim_command("normal! " .. line_number .. "G")
+-- 				break
+-- 			end
+-- 			if line_number < cursor_line and direction == "up" then
+-- 				vim.api.nvim_command("normal! " .. line_number .. "G")
+-- 			end
+-- 		end
+-- 	end
+-- end
+-- vim.api.nvim_set_keymap("n", "gp", "<cmd>lua Goto_hunk('up')<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "gn", "<cmd>lua Goto_hunk('down')<CR>", opts)
--- lua/user/utils/init.lua
+++ lua/user/utils/init.lua
@@ -1,11 +1,11 @@
 require("user.utils.constants")
 require("user.utils.custom_alternate")
-require("user.utils.custom_git")
+-- require("user.utils.custom_git")
 require("user.utils.custom_session_manager")
-require("user.utils.custom_statusline")
-require("user.utils.custom_winbar")
+-- require("user.utils.custom_statusline")
+-- require("user.utils.custom_winbar")
 require("user.utils.luasnip")
-require("user.utils.netrw")
+-- require("user.utils.netrw")
 require("user.utils.nvim_telescope")
 require("user.utils.autocommands")
 
@@ -19,18 +19,18 @@ require("user.utils.autocommands")
 -- custom null_ls/none-ls feature and extra_args = get_eslint_rules() which is responsible for long startup time
 
 -- netrw
--- vim.g.loaded_netrwPlugin = 1
--- vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
 -- statusline and winbar
--- local dark_gray = "#282a36"
--- local green = "#50fa7b"
--- local red = "#ff5555"
--- local light_purple = "#bd93f9"
--- local bright_orange = "#fe8019"
--- vim.api.nvim_set_hl(0, "MsgArea", { bg = dark_gray, fg = green })
--- vim.api.nvim_set_hl(0, "ErrorMsg", { bg = dark_gray, fg = red })
--- vim.api.nvim_set_hl(0, "StatusLine", { bg = dark_gray, fg = bright_orange })
--- vim.api.nvim_set_hl(0, "WinBar", { bg = dark_gray, fg = light_purple })
--- vim.api.nvim_set_hl(0, "CustomMod", { bg = green, fg = dark_gray })
+local dark_gray = "#282a36"
+local green = "#50fa7b"
+local red = "#ff5555"
+local light_purple = "#bd93f9"
+local bright_orange = "#fe8019"
+vim.api.nvim_set_hl(0, "MsgArea", { bg = dark_gray, fg = green })
+vim.api.nvim_set_hl(0, "ErrorMsg", { bg = dark_gray, fg = red })
+vim.api.nvim_set_hl(0, "StatusLine", { bg = dark_gray, fg = bright_orange })
+vim.api.nvim_set_hl(0, "WinBar", { bg = dark_gray, fg = light_purple })
+vim.api.nvim_set_hl(0, "CustomMod", { bg = green, fg = dark_gray })
 -- winbar
--- vim.o.winbar = "%=%#StatusLine# %y %#Winbar#%F %#CustomMod#%m%*%="
+vim.o.winbar = "%=%#StatusLine# %y %#Winbar#%F %#CustomMod#%m%*%="
