ef5bbbd36b8fedc1d07feded19b1a474a8924062
Joao Lobao <joao_lobao@sapo.pt>
2023-12-20 10:34:27 -0100

    moves tabline config to custom_qflist

--- lua/user/utils/autocommands.lua
+++ lua/user/utils/autocommands.lua
@@ -5,8 +5,8 @@ local group_quickfix = vim.api.nvim_create_augroup("CustomQFLMapping", { clear =
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "qf",
 	callback = function()
-		vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<CR>:cclose<CR>", opts)
-		vim.api.nvim_buf_set_keymap(0, "n", "<Esc>", ":cclose<CR>", opts)
+		vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<CR><cmd>lua Close_qf()<CR>", opts)
+		vim.api.nvim_buf_set_keymap(0, "n", "<Esc>", "<cmd>lua Close_qf()<CR>", opts)
 		vim.api.nvim_buf_set_keymap(0, "n", "f", "/", opts)
 		for i = 1, 10, 1 do
 			vim.api.nvim_buf_set_keymap(0, "n", tostring(i), tostring(i) .. "G<CR>:cclose<CR>", opts)
@@ -63,33 +63,6 @@ vim.cmd([[
   endfunction
   call AutoCorrect()
 ]])
-
--- statusline and tabline
-local dark_gray = "#282a36"
-local green = "#50fa7b"
-local red = "#ff5555"
-local light_purple = "#bd93f9"
-local bright_orange = "#fe8019"
-vim.api.nvim_set_hl(0, "MsgArea", { bg = dark_gray, fg = green })
-vim.api.nvim_set_hl(0, "QuickfixLine", { bg = "none", fg = "none" })
-vim.api.nvim_set_hl(0, "qfFileName", { bg = "none", fg = green })
-vim.api.nvim_set_hl(0, "ErrorMsg", { bg = dark_gray, fg = red })
-vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none", fg = bright_orange })
-vim.api.nvim_set_hl(0, "StatusLine", { bg = dark_gray, fg = bright_orange })
-vim.api.nvim_set_hl(0, "WinBar", { bg = dark_gray, fg = bright_orange })
-vim.api.nvim_set_hl(0, "TabLineFill", { bg = dark_gray, fg = light_purple })
-vim.api.nvim_set_hl(0, "CustomMod", { bg = green, fg = dark_gray })
-vim.o.showtabline = 2
-local is_git_repo = "system('git -C  . rev-parse --is-inside-work-tree') == 'true\n'"
-local pwd = "%{fnamemodify('', ':p:h')}"
-local file = "%#StatusLine#/%f"
-local modified = "%=%#CustomMod#%{&modified ? ' 󰆓 ' : ''}"
-local session = "%#WinBar# %{fnamemodify(v:this_session, ':t')} "
-local git_branch = "%#MsgArea#%{" .. is_git_repo .. " ? system('git -C . branch --show-current')[0:-2] : '-'} "
-local git_message = "%#TabLineFill#%{"
-	.. is_git_repo
-	.. " ? substitute(system('git -C . show -s --format=%s'), '\n', '', '')[0:50] : 'Not a git repo'}"
-vim.o.tabline = pwd .. file .. modified .. session .. git_branch .. git_message
 
 --TODO: features removed
 --custom_git
--- lua/user/utils/custom_qflist.lua
+++ lua/user/utils/custom_qflist.lua
@@ -9,17 +9,49 @@ local function is_any_quickfix_open()
 	end
 	return false
 end
+-- tabline
+local dark_gray = "#282a36"
+local green = "#50fa7b"
+local red = "#ff5555"
+local light_purple = "#bd93f9"
+local bright_orange = "#fe8019"
+vim.api.nvim_set_hl(0, "MsgArea", { bg = dark_gray, fg = green })
+vim.api.nvim_set_hl(0, "QuickfixLine", { bg = "none", fg = "none" })
+vim.api.nvim_set_hl(0, "qfFileName", { bg = "none", fg = green })
+vim.api.nvim_set_hl(0, "ErrorMsg", { bg = dark_gray, fg = red })
+vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none", fg = bright_orange })
+vim.api.nvim_set_hl(0, "StatusLine", { bg = dark_gray, fg = bright_orange })
+vim.api.nvim_set_hl(0, "WinBar", { bg = dark_gray, fg = bright_orange })
+vim.api.nvim_set_hl(0, "TabLineFill", { bg = dark_gray, fg = light_purple })
+vim.api.nvim_set_hl(0, "CustomMod", { bg = green, fg = dark_gray })
+local is_git_repo = "system('git -C  . rev-parse --is-inside-work-tree') == 'true\n'"
+local modified = "%#CustomMod#%{&modified ? '  󰆓 ' : ''}%*"
+local pwd = "%{fnamemodify('', ':p:h')}"
+local file = "%#StatusLine#/%f"
+local session = "%=%#WinBar# %{fnamemodify(v:this_session, ':t')} "
+local git_branch = "%#MsgArea#%{" .. is_git_repo .. " ? system('git -C . branch --show-current')[0:-2] : '-'} "
+local git_message = "%#TabLineFill#%{"
+	.. is_git_repo
+	.. " ? substitute(system('git -C . show -s --format=%s'), '\n', '', '')[0:50] : 'Not a git repo'}"
+vim.o.showtabline = 2
+vim.o.tabline = modified
+
 -- Toggle Quickfix window
+function Close_qf()
+	vim.o.tabline = modified
+	vim.cmd("cclose")
+end
 BufferName = function()
-  local buf_name = vim.fn.expand("%:f")
+	local buf_name = vim.fn.expand("%:f")
 	if is_any_quickfix_open() then
-		vim.cmd("cclose")
+		Close_qf()
 		return
 	end
 	local opened_bufs = vim.fn.getbufinfo({ buflisted = 1 })
 	vim.fn.setqflist(opened_bufs)
 	vim.cmd("top copen " .. #opened_bufs)
-  vim.fn.search(buf_name)
+	vim.o.tabline = pwd .. file .. session .. git_branch .. git_message
+	vim.fn.search(buf_name)
 end
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "<leader>n", "<cmd>lua BufferName()<CR>", opts)
