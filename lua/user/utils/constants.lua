d2606f02da2528204d2a349a57f26164d47937a9
Joao Lobao <joao_lobao@sapo.pt>
2023-12-20 15:07:33 -0100

    removes tabline and moves it to the statusline

--- lua/user/utils/constants.lua
+++ lua/user/utils/constants.lua
@@ -6,6 +6,17 @@ local hl_categories = {
 	vcs = "TelescopeResultsFunction",
 }
 
+Dark_gray = "#282a36"
+Light_purple = "#bd93f9"
+Dark_purple = "#6272a4"
+Cyan = "#8be9fd"
+Green = "#50fa7b"
+Orange = "#ffb86c"
+Pink = "#ff79c6"
+Red = "#ff5555"
+Bright_orange = "#fe8019"
+Gray = "#bbbbbb"
+White = "#ffffff"
 Session_dir = "~/.config/nvim/session/"
 Icons = {
 	folder = "📂",
--- lua/user/utils/custom_qflist.lua
+++ lua/user/utils/custom_qflist.lua
@@ -9,32 +9,35 @@ local function is_any_quickfix_open()
 	end
 	return false
 end
--- tabline
-local dark_gray = "#282a36"
-local green = "#50fa7b"
-local red = "#ff5555"
-local light_purple = "#bd93f9"
-local bright_orange = "#fe8019"
-vim.api.nvim_set_hl(0, "MsgArea", { bg = dark_gray, fg = green })
+-- general highlights
+vim.api.nvim_set_hl(0, "MsgArea", { bg = Dark_gray, fg = Green })
 vim.api.nvim_set_hl(0, "QuickfixLine", { bg = "none", fg = "none" })
-vim.api.nvim_set_hl(0, "qfFileName", { bg = "none", fg = green })
-vim.api.nvim_set_hl(0, "ErrorMsg", { bg = dark_gray, fg = red })
-vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none", fg = bright_orange })
-vim.api.nvim_set_hl(0, "StatusLine", { bg = dark_gray, fg = bright_orange })
-vim.api.nvim_set_hl(0, "WinBar", { bg = dark_gray, fg = bright_orange })
-vim.api.nvim_set_hl(0, "TabLineFill", { bg = dark_gray, fg = light_purple })
-vim.api.nvim_set_hl(0, "CustomMod", { bg = green, fg = dark_gray })
+vim.api.nvim_set_hl(0, "qfFileName", { bg = "none", fg = Bright_orange })
+vim.api.nvim_set_hl(0, "ErrorMsg", { bg = Dark_gray, fg = Red })
+vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none", fg = Bright_orange })
+vim.api.nvim_set_hl(0, "StatusLine", { bg = Dark_gray, fg = Light_purple })
+-- statusline highlights
+vim.api.nvim_set_hl(0, "StatusA", { bg = Dark_gray, fg = Dark_purple })
+vim.api.nvim_set_hl(0, "StatusB", { bg = Dark_gray, fg = Light_purple })
+vim.api.nvim_set_hl(0, "StatusC", { bg = Dark_gray, fg = Pink })
+vim.api.nvim_set_hl(0, "StatusD", { bg = Dark_gray, fg = Bright_orange })
+vim.api.nvim_set_hl(0, "StatusModified", { bg = Green, fg = Dark_gray })
+vim.api.nvim_set_hl(0, "StatusE", { bg = Dark_gray, fg = Orange })
+vim.api.nvim_set_hl(0, "StatusF", { bg = Dark_gray, fg = Cyan })
 local is_git_repo = "system('git -C  . rev-parse --is-inside-work-tree') == 'true\n'"
-local pwd = "%{fnamemodify('', ':p:h')}"
-local file = "%#StatusLine#/%f"
-local modified = "%=%#CustomMod#%{&modified ? ' 󰆓 ' : ''}"
-local session = "%#WinBar# %{fnamemodify(v:this_session, ':t')} "
-local git_branch = "%#MsgArea#%{" .. is_git_repo .. " ? system('git -C . branch --show-current')[0:-2] : '-'} "
-local git_message = "%#TabLineFill#%{"
+-- statusline components
+local ft = "%#StatusA#%y "
+local pwd = "%#StatusB#%{fnamemodify('', ':p:h')}"
+local file = "%#StatusD#/%f"
+local modified = "%=%#StatusModified#%{&modified ? ' 󰆓 ' : ''}"
+local git_branch = "%#MsgArea# %{" .. is_git_repo .. " ? system('git -C . branch --show-current')[0:-2] : '-'} "
+local git_message = "%#StatusC#%{"
 	.. is_git_repo
 	.. " ? substitute(system('git -C . show -s --format=%s'), '\n', '', '')[0:50] : 'Not a git repo'}"
-vim.o.showtabline = 2
-vim.o.tabline = pwd .. file .. modified .. session .. git_branch .. git_message
+local session = "%#StatusE# %{fnamemodify(v:this_session, ':t')} "
+local lines_cols = "%#StatusA#l%l:c%c"
+local total_lines = "%#StatusF# L:%L"
+vim.o.statusline = ft .. pwd .. file .. modified .. git_branch .. git_message .. session .. lines_cols .. total_lines
 
 -- Toggle Quickfix window
 BufferName = function()
@@ -45,7 +48,7 @@ BufferName = function()
 	end
 	local opened_bufs = vim.fn.getbufinfo({ buflisted = 1 })
 	vim.fn.setqflist(opened_bufs)
-	vim.cmd("top copen " .. #opened_bufs)
+	vim.cmd("copen " .. #opened_bufs)
 	vim.fn.search(buf_name)
 end
 local opts = { noremap = true, silent = true }
