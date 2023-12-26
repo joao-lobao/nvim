f3c3a5a0c587296f12f5aa36478e5683eeefbac0
Joao Lobao <joao_lobao@sapo.pt>
2023-12-26 11:42:23 -0100

    changes telescope keymaps

--- after/plugin/nvim_telescope.lua
+++ after/plugin/nvim_telescope.lua
@@ -58,6 +58,7 @@ local opts = { noremap = true, silent = true }
 
 -- find git files
 vim.api.nvim_set_keymap("n", "<leader>gf", "<Cmd>Telescope git_files<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader><leader>", "<Cmd>Telescope git_files<CR>", opts)
 -- find all files (doesn't respect .gitignore)
 vim.api.nvim_set_keymap("n", "<leader>F", "<Cmd>Telescope find_files no_ignore=true hidden=true<CR>", opts)
 -- grep respecting .gitignore
@@ -70,7 +71,7 @@ vim.api.nvim_set_keymap("n", "<leader>~", "<Cmd>Telescope search_files_in_home<C
 vim.api.nvim_set_keymap("n", "<leader>m", "<Cmd>Telescope common_actions<CR>", {})
 
 vim.api.nvim_set_keymap("n", "<leader>y", "<Cmd>Telescope registers<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader><leader>", "<Cmd>Telescope buffers<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>B", "<Cmd>Telescope buffers<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>M", "<Cmd>Telescope marks<CR>", opts)
 -- find in current buffer
 vim.api.nvim_set_keymap("n", "<leader>/", "<Cmd>Telescope current_buffer_fuzzy_find<CR>", opts)
