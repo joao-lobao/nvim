cbb2e2ac90e0f6beeac35516b3aa41936fc174ed
Joao Lobao <joao_lobao@sapo.pt>
2024-01-21 21:41:11 -0100

    Revert "removes telescope"
    This reverts commit d593ec72945a6e32f7d3eaffefe747cd6d3a3fe2.

--- lua/user/utils/constants.lua
+++ lua/user/utils/constants.lua
@@ -1,3 +1,11 @@
+local hl_categories = {
+	command = "TelescopeResultsVariable",
+	workspace = "TelescopeResultsIdentifier",
+	bookmark = "TelescopeResultsBookmark",
+	search = "TelescopeResultsConstant",
+	vcs = "TelescopeResultsFunction",
+}
+
 Dark_gray = "#282a36"
 Light_purple = "#bd93f9"
 Dark_purple = "#6272a4"
@@ -10,6 +18,16 @@ Bright_orange = "#fe8019"
 Gray = "#bbbbbb"
 White = "#ffffff"
 Session_dir = "~/.config/nvim/session/"
+Icons = {
+	folder = "📂",
+	opened_session = "📖",
+	closed_session = "📓",
+	config = "⚙️ ",
+	files = "🗃",
+	keymap = "🅰 ",
+	git = " ",
+	santa = "🎅",
+}
 function Get_sessions_names(pattern)
 	local sessions = vim.fn.glob(Session_dir .. "*", true, true)
 	local sessions_names = {}
@@ -22,3 +40,52 @@ function Get_sessions_names(pattern)
 	end
 	return sessions_names
 end
+
+-- put actions here
+local vim_cmds = {
+	{ icon = "e", description = "Empty buffer", value = "enew", category = hl_categories.command },
+	{ icon = "q", description = "Quit", value = "q", category = hl_categories.command },
+}
+local sessions_actions = function()
+	local sessions = vim.fn.glob(Session_dir .. "*", true, true)
+	local actions = {}
+	for _, session in ipairs(sessions) do
+		local session_name = vim.fn.fnamemodify(session, ":t")
+		table.insert(actions, {
+			icon = Icons.closed_session,
+			description = session_name,
+			value = "SLoad " .. session_name,
+			category = hl_categories.workspace,
+		})
+	end
+	return actions
+end
+local config_files = {
+	{ icon = Icons.config, description = "~/.tmux.conf", value = "e ~/.tmux.conf", category = hl_categories.bookmark },
+	{ icon = Icons.config, description = "~/.zshrc", value = "e ~/.zshrc", category = hl_categories.bookmark },
+}
+local searches = {
+	{ icon = Icons.files, description = "Old files", value = "Telescope oldfiles", category = hl_categories.search },
+	{ icon = Icons.keymap, description = "Keymaps", value = "Telescope keymaps", category = hl_categories.search },
+}
+local git_cmds = {
+	{ icon = Icons.git, description = "git commit", value = "Git commit", category = hl_categories.vcs },
+	{
+		icon = Icons.git,
+		description = "git commit --amend",
+		value = "Git commit --amend",
+		category = hl_categories.vcs,
+	},
+	{ icon = Icons.git, description = "git push", value = "Git push", category = hl_categories.vcs },
+	{ icon = Icons.git, description = "git push --force", value = "Git push --force", category = hl_categories.vcs },
+	{ icon = Icons.git, description = "git log %", value = "Gclog -- %", category = hl_categories.vcs },
+	{ icon = Icons.git, description = "git log last commit", value = "GitLastCommit", category = hl_categories.vcs },
+}
+
+Common_actions = {}
+local groups = { vim_cmds, sessions_actions(), config_files, searches, git_cmds }
+table.foreach(groups, function(_, group)
+	table.foreach(group, function(_, item)
+		table.insert(Common_actions, item)
+	end)
+end)
