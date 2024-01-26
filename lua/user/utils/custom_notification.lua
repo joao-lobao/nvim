4a1b3f7a771cffdcd0a4fa437d975e1fa459b866
Joao Lobao <joao_lobao@sapo.pt>
2024-01-26 05:40:38 -0100

    fixes notifications when buffer has none or few lines

--- lua/user/utils/custom_notification.lua
+++ lua/user/utils/custom_notification.lua
@@ -1,47 +1,39 @@
 local namespace = vim.api.nvim_create_namespace("session-notify")
 local config = {
 	[vim.log.levels.INFO] = {
-		icon = "",
+		icon = "      ",
 		hl = "NotificationInfo",
 		hl_emphasis = "NotificationInfoEmphasis",
 	},
 	[vim.log.levels.WARN] = {
-		icon = "",
+		icon = "      ",
 		hl = "NotificationWarn",
 		hl_emphasis = "NotificationWarnEmphasis",
 	},
 	[vim.log.levels.ERROR] = {
-		icon = "",
+		icon = "      ",
 		hl = "NotificationError",
 		hl_emphasis = "NotificationErrorEmphasis",
 	},
 }
 
 function Notification(message, level, emphasis, timeout)
-	local txt_emphasis = ""
-	if emphasis ~= nil then
-		txt_emphasis = emphasis .. " "
-	end
-	if timeout == nil then
-		timeout = 5000
-	end
+	emphasis = emphasis ~= nil and emphasis .. " " or ""
+	timeout = timeout ~= nil and timeout or 5000
 	local buffer = vim.api.nvim_get_current_buf()
-	local txt_icon = "    " .. config[level].icon .. "  "
-	local txt_msg = message .. "    "
 	local extmarks = vim.api.nvim_buf_get_extmarks(buffer, namespace, 0, -1, {})
-	local id = 1
-
 	-- id working also as an offset when there will be multiple active notifications
-	if #extmarks > 0 then
-		id = extmarks[#extmarks][1] + 2
-	end
+	local id = #extmarks > 0 and extmarks[#extmarks][1] + 2 or 1
+	local line_count = vim.api.nvim_buf_line_count(buffer)
 	local screen_topline = vim.fn.line("w0") - 2 + id
+	-- check notification position, higher than buffer size
+	screen_topline = screen_topline > line_count and 0 or screen_topline
 	vim.api.nvim_buf_set_extmark(buffer, namespace, screen_topline, 0, {
 		id = id,
 		virt_text = {
-			{ txt_icon, config[level].hl },
-			{ txt_emphasis, config[level].hl_emphasis },
-			{ txt_msg, config[level].hl },
+			{ config[level].icon, config[level].hl },
+			{ emphasis, config[level].hl_emphasis },
+			{ message .. "    ", config[level].hl },
 		},
 		virt_text_pos = "right_align",
 		priority = 50,
