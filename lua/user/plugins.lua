0a8f20a62b03b1729f88d66c812bc05d6ff57ae4
Joao Lobao <joao_lobao@sapo.pt>
2024-11-19 23:02:11 -0100

    feat(plugins): add CopilotChat.nvim plugin
    Added CopilotChat.nvim plugin to enhance chat capabilities. This plugin
    is added with necessary dependencies and build instructions for Linux
    and MacOS. Configuration options are also included for future
    customization.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -60,6 +60,19 @@ require("lazy").setup({
 			winblend = 100, -- winblend for the drop window
 		},
 	}, -- animation/screensaver
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
 
 	-- NOTE: Now, plugins that require configuration
 	-- LSP
