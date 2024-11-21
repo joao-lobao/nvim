7ceab8f0214bcb6d30f78565bb184ac36c346984
Joao Lobao <joao_lobao@sapo.pt>
2024-11-21 08:27:12 -0100

    refactor(CopChat): changes on keymaps and setup
    removes keymappings and adds setup function for the plugin to add 2
    custom prompts

--- after/plugin/copilot_chat.lua
+++ after/plugin/copilot_chat.lua
@@ -1,12 +1,6 @@
 local chat = require("CopilotChat")
 vim.keymap.set({ "n", "v" }, "<leader>ai", chat.toggle, { desc = "AI Toggle" })
-vim.keymap.set({ "n", "v" }, "<leader>ac", ":CopilotChatCommit<CR>", { desc = "AI Commit" })
-vim.keymap.set({ "n", "v" }, "<leader>ae", ":CopilotChat Explain<CR>", { desc = "AI Explain" })
-vim.keymap.set({ "n", "v" }, "<leader>af", ":CopilotChat Fix<CR>", { desc = "AI Fix" })
-vim.keymap.set({ "n", "v" }, "<leader>ao", ":CopilotChat Optimize<CR>", { desc = "AI Optimize" })
-vim.keymap.set({ "n", "v" }, "<leader>ar", ":CopilotChat Review<CR>", { desc = "AI Review" })
 vim.keymap.set({ "n" }, "<leader>ad", chat.reset, { desc = "AI Reset" })
-vim.keymap.set({ "n", "v" }, "<leader>at", ":CopilotChat Tests<CR>", { desc = "AI Tests" })
 -- AI ask
 vim.keymap.set({ "n", "v" }, "<leader>aa", function()
 	vim.ui.input({
@@ -17,3 +11,14 @@ vim.keymap.set({ "n", "v" }, "<leader>aa", function()
 		end
 	end)
 end, { desc = "AI Quick Chat" })
+
+chat.setup({
+	prompts = {
+		BetterNamings = {
+			prompt = "> /COPILOT_REVIEW\n\nPlease suggest better names for the variables and functions in the selected code.",
+		},
+		Refactor = {
+			prompt = "> /COPILOT_GENERATE\n\nRefactor the selected code.",
+		},
+	},
+})
