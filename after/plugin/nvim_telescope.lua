8bea831d37d0c58171f1aed993a65f1537221407
Joao Lobao <joao_lobao@sapo.pt>
2024-11-29 04:32:35 -0100

    refactor(Telescope): new mappings for Telescope
    reduces Telescope mappings now replaced by the actions in the new custom
    pickers

--- after/plugin/nvim_telescope.lua
+++ after/plugin/nvim_telescope.lua
@@ -81,23 +81,8 @@ end
 local opts = { noremap = true, silent = true }
 
 -- CopilotChat actions
-vim.api.nvim_set_keymap("n", "ta", "<Cmd>Telescope CopilotChat<CR>", opts)
--- find git/project files in cwd
-vim.api.nvim_set_keymap("n", "tf", "<Cmd>Telescope find_in_cwd<CR>", opts)
--- find all files in cwd
-vim.api.nvim_set_keymap("n", "tF", "<Cmd>Telescope find_files no_ignore=true hidden=true<CR>", opts)
--- grep git/project in cwd
-vim.api.nvim_set_keymap("n", "tg", "<Cmd>Telescope grep_in_cwd<CR>", opts)
--- grep all files in cwd
-vim.api.nvim_set_keymap("n", "tG", "<Cmd>Telescope grep_all<CR>", opts)
-
--- find files from home dir as cwd
-vim.api.nvim_set_keymap("n", "t~", "<Cmd>Telescope search_files_in_home<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>a", "<Cmd>Telescope CopilotChat<CR>", opts)
+-- Telescope find actions
+vim.api.nvim_set_keymap("n", "<leader>r", "<Cmd>Telescope Find<CR>", opts)
 -- common or useful commands
-vim.api.nvim_set_keymap("n", "tm", "<Cmd>Telescope common_actions<CR>", {})
-
-vim.api.nvim_set_keymap("n", "ty", "<Cmd>Telescope registers<CR>", opts)
-vim.api.nvim_set_keymap("n", "to", "<Cmd>Telescope oldfiles<CR>", opts)
-vim.api.nvim_set_keymap("n", "tn", "<Cmd>Telescope buffers<CR>", opts)
-vim.api.nvim_set_keymap("n", "tM", "<Cmd>Telescope marks<CR>", opts)
-vim.api.nvim_set_keymap("n", "tk", "<Cmd>Telescope keymaps<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>n", "<Cmd>Telescope common_actions<CR>", {})
