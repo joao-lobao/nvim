a0909240da62ec71fd60c84527b730d1d771cbfa
Joao Lobao <joao_lobao@sapo.pt>
2024-12-27 05:09:41 -0100

    feat(treesitter): adds repeatability power moves through textobjs
    - adds reapeatable moves through treesitter-textobjects functionality,
      enabling reapeatable moves/jumps through "f", "F", "t", "T" keys

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -93,3 +93,15 @@ treesitter.setup({
 		lint_events = { "BufWrite", "CursorHold" },
 	},
 })
+local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+
+-- Creates Super Repeatable Move functionality. Repeat movement with ; and ,
+-- ensure ; goes forward and , goes backward regardless of the last direction
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+-- make builtin f, F, t, T also repeatable with ; and ,
+vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true })
+vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true })
+-- also many more functionalities are available, see https://github.com/nvim-treesitter/nvim-treesitter-textobjects
