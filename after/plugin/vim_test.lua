cd1fbcbb326e40ecacf5252af3a5061925069825
Joao Lobao <joao_lobao@sapo.pt>
2025-01-03 18:54:49 -0100

    feat(vim-test): removes unused keymaps

--- after/plugin/vim_test.lua
+++ after/plugin/vim_test.lua
@@ -1,8 +1,6 @@
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap("n", "<leader>tt", ":TestNearest<CR>", opts)
 vim.api.nvim_set_keymap("n", "<leader>tf", ":TestFile<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>ts", ":TestSuite<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>t$", ":TestLast<CR>", opts)
 
 vim.g["test#strategy"] = "neovim"
 vim.g["test#neovim#term_position"] = "vertical"
