46525bf3bf20401e9eb520008c7784f3654b0fc6
Joao Lobao <joao_lobao@sapo.pt>
2025-01-16 05:22:29 -0100

    refactor(copilot): changed dismiss suggestion keymap

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -2,5 +2,5 @@ vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 vim.api.nvim_set_keymap("i", "<M-j>", "<Plug>(copilot-next)", { noremap = false })
 vim.api.nvim_set_keymap("i", "<M-k>", "<Plug>(copilot-previous)", { noremap = false })
-vim.api.nvim_set_keymap("i", "<C-e>", "<Plug>(copilot-dismiss)", { noremap = false })
+vim.api.nvim_set_keymap("i", "<C-d>", "<Plug>(copilot-dismiss)", { noremap = false })
 vim.api.nvim_set_keymap("i", "<C-s>", "<Plug>(copilot-suggest)", { noremap = false })
