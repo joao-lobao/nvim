6a377e176da0a7ad23d315fe7143d6350f2b5804
Joao Lobao <joao_lobao@sapo.pt>
2025-01-24 09:39:30 -0100

    refactor(notification): set initial notification one line bellow

--- lua/user/utils/custom_notification.lua
+++ lua/user/utils/custom_notification.lua
@@ -22,8 +22,8 @@ function Notification(message, level, emphasis, timeout, notify_vim)
 	timeout = timeout ~= nil and timeout or 5000
 	local buffer = vim.api.nvim_get_current_buf()
 	local extmarks = vim.api.nvim_buf_get_extmarks(buffer, namespace, 0, -1, {})
-	-- id working also as an offset when there will be multiple active notifications
-	local id = #extmarks > 0 and extmarks[#extmarks][1] + 2 or 1
+	-- id working also as an offset when there will be multiple active notifications, or set to line 2 if there are none
+	local id = #extmarks > 0 and extmarks[#extmarks][1] + 2 or 2
 	local line_count = vim.api.nvim_buf_line_count(buffer)
 	local screen_topline = vim.fn.line("w0") - 2 + id
 	-- check notification position, higher than buffer size
