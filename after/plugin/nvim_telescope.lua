7c06434a121e28db01ed36b5d21cda396a793a8e
Joao Lobao <joao_lobao@sapo.pt>
2025-01-27 02:42:11 -0100

    refactor(telescope,qflist): changed search keymaps
    feat(search): commented some qflist and telescope keymaps
    
    feat(telescope): added diagnostics keymap

--- after/plugin/nvim_telescope.lua
+++ after/plugin/nvim_telescope.lua
@@ -77,7 +77,7 @@ end
 local opts = { noremap = true, silent = true }
 
 -- Actions
-vim.api.nvim_set_keymap("n", "tm", "<Cmd>Telescope common_actions<CR>", {})
+vim.api.nvim_set_keymap("n", "ts", "<Cmd>Telescope common_actions<CR>", {})
 vim.api.nvim_set_keymap("n", "ta", "<Cmd>Telescope copilot_chat<CR>", opts)
 -- Find files
 vim.api.nvim_set_keymap("n", "tf", "<Cmd>Telescope find_in_cwd<CR>", opts)
@@ -88,9 +88,10 @@ vim.api.nvim_set_keymap("n", "tg", "<Cmd>Telescope grep_in_cwd<CR>", opts)
 vim.api.nvim_set_keymap("n", "tG", "<Cmd>Telescope grep_all<CR>", opts)
 -- Telescope
 vim.api.nvim_set_keymap("n", "to", "<Cmd>Telescope oldfiles<CR>", opts)
-vim.api.nvim_set_keymap("n", "tb", "<Cmd>Telescope buffers<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "tb", "<Cmd>Telescope buffers<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "td", "<Cmd>Telescope diagnostics<CR>", opts)
 vim.api.nvim_set_keymap("n", "tk", "<Cmd>Telescope keymaps<CR>", opts)
 vim.api.nvim_set_keymap("n", "tr", "<Cmd>Telescope registers<CR>", opts)
-vim.api.nvim_set_keymap("n", "tM", "<Cmd>Telescope marks<CR>", opts)
+vim.api.nvim_set_keymap("n", "tm", "<Cmd>Telescope marks<CR>", opts)
 vim.api.nvim_set_keymap("n", "tcc", "<Cmd>Telescope git_commits<CR>", opts)
 vim.api.nvim_set_keymap("n", "tcb", "<Cmd>Telescope git_bcommits<CR>", opts)
--- lua/user/utils/custom_qflist.lua
+++ lua/user/utils/custom_qflist.lua
@@ -138,18 +138,18 @@ end
 
 local opts = { noremap = true, silent = false }
 -- git/project files
-vim.api.nvim_set_keymap("n", "<leader><leader>f", "<cmd>lua Files('project')<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "<leader><leader>f", "<cmd>lua Files('project')<CR>", opts)
 -- all files
-vim.api.nvim_set_keymap("n", "<leader><leader>F", "<cmd>lua Files('all')<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "<leader><leader>F", "<cmd>lua Files('all')<CR>", opts)
 -- git grep
-vim.api.nvim_set_keymap("n", "<leader><leader>g", "<cmd>lua Grep()<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "<leader><leader>g", "<cmd>lua Grep()<CR>", opts)
 -- all grep
-vim.api.nvim_set_keymap("n", "<leader><leader>G", "<cmd>lua Grep('--no-ignore')<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "<leader><leader>G", "<cmd>lua Grep('--no-ignore')<CR>", opts)
 -- others
-vim.api.nvim_set_keymap("n", "<leader><leader>b", "<cmd>lua ListedBuffers()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>o", "<cmd>lua Oldfiles()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>d", "<cmd>lua Diagnostics()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>E", "<cmd>lua Eslint_to_qflist()<CR>", opts)
+vim.api.nvim_set_keymap("n", "tb", "<cmd>lua ListedBuffers()<CR>", opts)
+-- vim.api.nvim_set_keymap("n", "<leader>o", "<cmd>lua Oldfiles()<CR>", opts)
+vim.api.nvim_set_keymap("n", "td", "<cmd>lua Diagnostics()<CR>", opts)
+vim.api.nvim_set_keymap("n", "tE", "<cmd>lua Eslint_to_qflist()<CR>", opts)
 -- open quickfix list
 vim.api.nvim_set_keymap("n", "co", ":copen<CR>", opts)
 vim.api.nvim_set_keymap("n", "cc", ":cclose<CR>", opts)
