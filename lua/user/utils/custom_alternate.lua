09610733df868a6026826375d5fff9032b618f10
Joao Lobao <joao_lobao@sapo.pt>
2025-02-13 21:09:00 -0100

    refactor(alternate): alternate more simple and flexible

--- lua/user/utils/custom_alternate.lua
+++ lua/user/utils/custom_alternate.lua
@@ -19,12 +19,8 @@ local create_alternate_file = function(alternate_type)
 
 	local fname_no_ext = string.gsub(vim.fn.expand("%:t"), "%..*$", "")
 	local directory = vim.fn.expand("%:p:h")
-	local ext = vim.fn.expand("%:e")
 
 	local alternate_file = fname_no_ext .. "." .. alternate_type
-	if alternate_type == "test" then
-		alternate_file = fname_no_ext .. "." .. alternate_type .. "." .. ext
-	end
 
 	-- ask with a prompt in case user wants to create alternate file
 	local is_create_file = vim.fn.input("Create " .. alternate_type .. " file? (y/n) ")
@@ -38,30 +34,28 @@ local create_alternate_file = function(alternate_type)
 	end
 end
 
-local goto_alternate = function(alternate_type)
+SwitchToAlternate = function(main_alter_patt, sec_alter_patt, new_file_type)
 	local file = vim.fn.expand("%:t")
-	local ext = vim.fn.expand("%:e")
-	local pattern = ".[j|t]s.*"
+	local pattern = sec_alter_patt
 
-	-- check for specific filetype css or test files
-	if alternate_type:match("css") then
-		pattern = ".*css"
-	elseif alternate_type:match("test") and not (file:match(".*[t|s][e|p][s|e][t|c].*") or ext:match(".*css")) then
-		pattern = ".*[t|s][e|p][s|e][t|c].*"
+	-- file name different from secondary alternate pattern, set pattern to find main file
+	if file:match(sec_alter_patt) then
+		pattern = main_alter_patt
 	end
 
 	local source_file = find_alternate_file(pattern)
 	if #source_file > 0 then
 		vim.cmd("edit " .. source_file[1])
 	else
-		create_alternate_file(alternate_type)
+		create_alternate_file(new_file_type)
 	end
 end
 
-SwitchToAlternate = function(alternate_type)
-	goto_alternate(alternate_type)
-end
-
 local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap("n", "<leader>A", "<cmd>lua SwitchToAlternate('test')<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>C", "<cmd>lua SwitchToAlternate('css')<CR>", opts)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>A",
+	"<cmd>lua SwitchToAlternate('.[j|t]s.*', '.*[t|s][e|p][s|e][t|c].*', 'test.ts')<CR>",
+	opts
+)
+vim.api.nvim_set_keymap("n", "<leader>C", "<cmd>lua SwitchToAlternate('.[j|t]s.*','.*css', 'css')<CR>", opts)
