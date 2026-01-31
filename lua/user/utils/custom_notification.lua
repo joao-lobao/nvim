40577eb8bada2e1863ca8fba8e61c5958b513260
Joao Lobao <joao_lobao@sapo.pt>
2026-01-31 07:29:56 -0100

    refactor(notification): simplifies notifications by removing animations

--- lua/user/utils/custom_notification.lua
+++ lua/user/utils/custom_notification.lua
@@ -18,32 +18,22 @@ local config = {
 }
 
 local notifications = {}
-local function set_ext_mark(modifier, col, buffer, screen_topline, id, icon, msg, level, total_length)
-	vim.fn.timer_start(10, function()
-		if vim.api.nvim_buf_is_loaded(buffer) then
-			if modifier == "show" then
-				col = col - 1
-			else
-				col = col + 1
-			end
-
-			vim.api.nvim_buf_set_extmark(buffer, namespace, screen_topline, 0, {
-				id = id,
-				virt_text = {
-					{ icon, config[level].icon_hl },
-					{ msg, config[level].hl },
-				},
-				virt_text_win_col = col,
-				priority = 50,
-			})
-		end
-	end, { ["repeat"] = total_length })
+local function set_ext_mark(buffer, screen_topline, id, icon, msg, level)
+	if vim.api.nvim_buf_is_loaded(buffer) then
+		vim.api.nvim_buf_set_extmark(buffer, namespace, screen_topline, 0, {
+			id = id,
+			virt_text = {
+				{ icon, config[level].icon_hl },
+				{ msg, config[level].hl },
+			},
+			virt_text_pos = "right_align",
+		})
+	end
 end
 
 function Notification(message, level)
 	local icon = " " .. config[level].icon .. " "
 	local msg = " " .. message .. " "
-	local total_length = #icon + #msg
 	local buffer = vim.api.nvim_get_current_buf()
 	local extmarks = vim.api.nvim_buf_get_extmarks(buffer, namespace, 0, -1, {})
 	-- id working also as an offset when there will be multiple active notifications, or set to line 0
@@ -52,20 +42,13 @@ function Notification(message, level)
 	local screen_topline = vim.fn.line("w0") - 2 + id
 	-- check notification position, higher than buffer size
 	screen_topline = screen_topline > line_count and 0 or screen_topline
-	local width = vim.api.nvim_win_get_width(0) - 7
 
-	-- animate notification (can also be achieved with loop timer)
 	-- show
-	set_ext_mark("show", width, buffer, screen_topline, id, icon, msg, level, total_length)
+	set_ext_mark(buffer, screen_topline, id, icon, msg, level)
 	-- hiding starts after 5 seconds
 	vim.fn.timer_start(5000, function()
-		set_ext_mark("hide", width - total_length, buffer, screen_topline, id, icon, msg, level, total_length)
-	end)
-
-	-- clean notification
-	vim.fn.timer_start(5100 + (total_length * 10), function()
-		-- if extmark still exists, delete it
-		if vim.api.nvim_buf_is_valid(buffer) then
+		-- hide
+		if vim.api.nvim_buf_is_loaded(buffer) then
 			vim.api.nvim_buf_del_extmark(buffer, namespace, id)
 		end
 	end)
