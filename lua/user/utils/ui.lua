c7066637d6740a9a876c0075df2d8bc287eb6f55
Joao Lobao <joao_lobao@sapo.pt>
2026-02-04 13:02:40 -0100

    refactor(qflist): changes warning notification colors

--- lua/user/utils/ui.lua
+++ lua/user/utils/ui.lua
@@ -4,8 +4,8 @@ vim.api.nvim_set_hl(0, "MsgArea", { bg = Dark_gray, fg = Green })
 vim.api.nvim_set_hl(0, "NotificationInfo", { bold = true, bg = Green, fg = Medium_gray })
 vim.api.nvim_set_hl(0, "NotificationInfoInv", { bold = true, bg = Medium_gray, fg = Green })
 --Warn
-vim.api.nvim_set_hl(0, "NotificationWarn", { bold = true, bg = Bright_orange, fg = Medium_gray })
-vim.api.nvim_set_hl(0, "NotificationWarnInv", { bold = true, bg = Medium_gray, fg = Bright_orange })
+vim.api.nvim_set_hl(0, "NotificationWarn", { bold = true, bg = Yellow, fg = Medium_gray })
+vim.api.nvim_set_hl(0, "NotificationWarnInv", { bold = true, bg = Medium_gray, fg = Yellow })
 --Error
 vim.api.nvim_set_hl(0, "NotificationError", { bold = true, bg = Red, fg = Medium_gray })
 vim.api.nvim_set_hl(0, "NotificationErrorInv", { bold = true, bg = Medium_gray, fg = Red })
