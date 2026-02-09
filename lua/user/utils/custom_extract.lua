22112bb85f0c18110a05f602959aa4398896f443
Joao Lobao <joao_lobao@sapo.pt>
2026-02-09 22:04:48 -0100

    refactor(extract): simplifies constants and adds flexibility
    now able to create an even more custom extraction by passing params to
    Extract method

--- lua/user/utils/custom_extract.lua
+++ lua/user/utils/custom_extract.lua
@@ -1,11 +1,3 @@
-local map_as = {
-	class = "1. as_class",
-	func = "2. as_function",
-	file_as_class = "3. to_file_as_class",
-	file_as_func = "4. to_file_as_function",
-	method = "5. as_method",
-}
-
 local feedkeys = function(extract_as)
 	vim.api.nvim_command("normal! k")
 	require("nvim-treesitter.textobjects.move").goto_next_end("@function.outer", "textobjects")
@@ -14,7 +6,7 @@ local feedkeys = function(extract_as)
 	vim.api.nvim_command("normal! p")
 end
 
-local extract = function(as, to)
+local create = function(as, to)
 	vim.cmd("normal! d")
 	if to == "file" then
 		local fname = vim.fn.input("Enter file name: ", vim.fn.expand("%:p:h"), "file")
@@ -23,40 +15,54 @@ local extract = function(as, to)
 	feedkeys(as)
 end
 
-local choose_extract = function(as)
-	local extraction_list = {
-		[map_as.class] = function()
-			extract("class Name {\n}")
+local map_as = {
+	[1] = {
+		name = "1. as_class",
+		extract = function()
+			create("class Name {\n}")
 		end,
-		[map_as.func] = function()
-			extract("function name() {\n}")
+	},
+	[2] = {
+		name = "2. as_function",
+		extract = function()
+			create("function name() {\n}")
 		end,
-		[map_as.file_as_class] = function()
-			extract("class Name {\n}", "file")
+	},
+	[3] = {
+		name = "3. to_file_as_class",
+		extract = function()
+			create("class Name {\n}", "file")
 		end,
-		[map_as.file_as_func] = function()
-			extract("function name() {\n}", "file")
+	},
+	[4] = {
+		name = "4. to_file_as_function",
+		extract = function()
+			create("function name() {\n}", "file")
 		end,
-		[map_as.method] = function()
-			extract("name() {\n}")
+	},
+	[5] = {
+		name = "5. as_method",
+		extract = function()
+			create("name() {\n}")
 		end,
-	}
+	},
+}
 
-	return extraction_list[as]()
-end
-
-Extract = function()
-	local as_map = { map_as.class, map_as.func, map_as.file_as_class, map_as.file_as_func, map_as.method }
-	local as = vim.fn.inputlist({
+Extract = function(as, to)
+	if as ~= nil then
+		create(as, to)
+		return
+	end
+	local pick = vim.fn.inputlist({
 		"Extract as: ",
-		map_as.class,
-		map_as.func,
-		map_as.file_as_class,
-		map_as.file_as_func,
-		map_as.method,
+		map_as[1].name,
+		map_as[2].name,
+		map_as[3].name,
+		map_as[4].name,
+		map_as[5].name,
 	})
-	if as ~= nil and as > 0 then
-		choose_extract(as_map[as])
+	if pick ~= nil and pick > 0 then
+		map_as[pick].extract()
 	end
 end
 
